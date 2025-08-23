d1518f28ece92633f8e9ac2ad23f965c077cf15e
Bidhu1024 <bidhu1024av@gmail.com>
2025-08-24 01:13:53 +0530

    upper section completed

--- src/components/Streched.tsx
+++ src/components/Streched.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import React, { type FC } from "react";
+import { Box, Link, Typography } from "@mui/material";
+import { type FC } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 
 interface Streched {
@@ -11,10 +11,126 @@ const Streched: FC<Streched> = ({ handleClick }) => {
       sx={{
         width: "100%",
         position: "relative",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      <Box sx={{ position: "absolute", top: 10, right: 10 }}>
-        <CloseIcon onClick={handleClick} />
+      <Box
+        sx={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          border: "1px solid black",
+          p: 1,
+        }}
+      >
+        <CloseIcon sx={{ fontSize: "2rem" }} onClick={handleClick} />
+      </Box>
+      <Box sx={{ width: "100%", display: "flex", mt: "6rem",p:1 }}>
+        <Box>
+          <Typography
+            fontFamily={"AdihausDIN, Helvetica, Arial, Helvetica, sans-serif"}
+            fontSize={"1rem"}
+            fontWeight={700}
+          >
+            SIGN UP & GET 10% OFF
+          </Typography>
+          <Typography
+            fontFamily={"AdihausDIN, Helvetica, Arial, Helvetica, sans-serif"}
+            fontSize={14}
+            fontWeight={400}
+          >
+            Members get more! Like a 10% discount voucher early access to the
+            sales and access to limited edition products. Sign up now
+          </Typography>
+          <Link
+            sx={{
+          //    textDecoration: "underlined",
+              color: "black",
+              fontWeight: "600",
+            }}
+          >
+            LEARN MORE
+          </Link>
+        </Box>
+        <Box>
+          <Typography
+            fontFamily={"AdihausDIN, Helvetica, Arial, Helvetica, sans-serif"}
+            fontSize={"1rem"}
+            fontWeight={700}
+          >
+            FREE DELIVERY, RETURN & EXCHANGE
+          </Typography>
+          <Typography
+            fontFamily={"AdihausDIN, Helvetica, Arial, Helvetica, sans-serif"}
+            fontSize={14}
+            fontWeight={400}
+          >
+            Spend over Rs1000/- and your delivery is FREE! For all orders,
+            Return & Exchange is offered for free. Delivery times for Delhi,
+            Gurgaon: 1-2 Days, Metro Cities: 2-3 Days Others: 3-5 Days.
+          </Typography>
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            gap="10px"
+            sx={{ mt: ".3rem" }}
+          >
+            <Link
+              sx={{
+              //  textDecoration: "underlined",
+                color: "black",
+                fontWeight: "600",
+              }}
+            >
+              READ MORE ON DELIVERY
+            </Link>
+            <Link
+              sx={{
+                textDecoration: "underlined",
+                color: "black",
+                fontWeight: "600",
+              }}
+            >
+              READ MORE ON RETURN
+            </Link>
+            <Link
+              sx={{
+                textDecoration: "underlined",
+                color: "black",
+                fontWeight: "600",
+              }}
+            >
+              READ MORE ON EXCHANGE
+            </Link>
+          </Box>
+        </Box>
+        <Box>
+          <Typography
+            fontFamily={"AdihausDIN, Helvetica, Arial, Helvetica, sans-serif"}
+            fontSize={"1rem"}
+            fontWeight={700}
+          >
+            STUDENTS GET 15% OFF
+          </Typography>
+          <Typography
+            fontFamily={"AdihausDIN, Helvetica, Arial, Helvetica, sans-serif"}
+            fontSize={14}
+            fontWeight={400}
+          >
+            For instant access to this discount simply join now and verify your
+            student status with UNiDAYS !
+          </Typography>
+          <Link
+            sx={{
+              textDecoration: "underlined",
+              color: "black",
+              fontWeight: "600",
+            }}
+          >
+            JOIN NOW
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,35 +1,50 @@
 import { Box, Typography } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Streched from "./Streched";
 
 const Topbar = () => {
   const [clicked, setClicked] = useState(false);
-const handleClose = ()=>{
-    setClicked(false)
-}
+  const [index, setIndex] = useState(0);
+  const texts = [
+    "FREE DELIVERY, RETURN & EXCHANGE",
+    "SIGN UP & GET 10% OFF",
+    "STUDENTS GET 15% OFF",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % texts.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+  const handleClose = () => {
+    setClicked(false);
+  };
   return (
     <>
       <Box
         sx={{
           width: "100%",
-          height: clicked ? "10rem" : "4rem",
+          minHeight: clicked ? "20rem" : "4rem",
           p: 2,
-          bgcolor: clicked ? "red" : "black",
+          bgcolor: clicked ? "white" : "black",
           cursor: "pointer",
           display: "flex",
           justifyContent: "center",
           gap: "1rem",
+          transition: "all 0.5s ease-in-out",
+          overflow: "hidden",
         }}
       >
         {clicked ? (
-          <Streched handleClick = {handleClose}/>
+          <Streched handleClick={handleClose} />
         ) : (
           <>
             <Typography
               sx={{ color: "white", fontWeight: "600", textAlign: "center" }}
             >
-              FREE DELIVERY, RETURN & EXCHANGE
+              {texts[index]}
             </Typography>
             <Box>
               <KeyboardArrowDownIcon
