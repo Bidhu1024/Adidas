6707165c14f977e75a9bc9e2d71af60c74027502
Bidhu1024 <bidhu1024av@gmail.com>
2025-08-25 20:44:13 +0530

    fix

---
+++ src/components/Shopping/ShopingSection.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+const ShopingSection = () => {
+  return <Box sx={{ height: "20rem", p: 2, width: "100%" }}>
+
+  </Box>
+};
+
+export default ShopingSection;
--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,19 +1,19 @@
-import ImageBox from '../../components/Image/ImageBox';
-import Navbar from '../../components/Navbar/Navbar';
-import Topbar from '../../components/Topbar';
-import VictoryBar from '../../components/Victory/VictoryBar';
-
+import ImageBox from "../../components/Image/ImageBox";
+import Navbar from "../../components/Navbar/Navbar";
+import ShopingSection from "../../components/Shopping/ShopingSection";
+import Topbar from "../../components/Topbar";
+import VictoryBar from "../../components/Victory/VictoryBar";
 
 const Dashboard = () => {
   return (
-    <div style={{display:"flex",flexDirection:"column",width:"100%"}}>
+    <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
       <Topbar />
       <Navbar />
       <VictoryBar />
-      <ImageBox/>
-
+      <ImageBox />
+      <ShopingSection />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
